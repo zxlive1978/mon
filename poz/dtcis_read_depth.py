cfc25e95132c7aa6f3b4fe65fb5d2efc533e59fd
zxlive1978 <zxlive@yandex.ru>
2020-12-25 15:25:58 +0400

    dsa

--- poz/dtcis_read_depth.py
+++ poz/dtcis_read_depth.py
@@ -717,7 +717,7 @@ def read_well(sbor,table):
 
 
 #544
-t544 = Process(target=read_well, args=["/mnt/544","s544d"])
+t544 = Process(target=read_well, args=["/mnt/544","s544depth"])
 t544.start()
 t544.join(180)
 if t544.is_alive(): t544.terminate()
