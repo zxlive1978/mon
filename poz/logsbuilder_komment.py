2b6bb2e57561aed2d2143858beaf45ee1b27593b
zxlive1978 <zxlive@yandex.ru>
2021-04-23 10:57:18 +0400

    ds

--- poz/logsbuilder_komment.py
+++ poz/logsbuilder_komment.py
@@ -58,10 +58,10 @@ def read_well(sbor,table):
 	pathtocopy=path_to_work+'WELLSITEDB'+table+'.gz'
 	print pathtocopy
 	# try:
-	# 	# subprocess.call("rm "+path_to_work+"WELLSITEDB", shell=True)
-	# 	shutil.copy(full_path_skf, pathtocopy)
+		# subprocess.call("rm "+path_to_work+"WELLSITEDB", shell=True)
+	shutil.copy(full_path_skf, pathtocopy)
 	# except IOError, e:
-	# 	print "Unable to copy file. %s" % e
+		# print "Unable to copy file. %s" % e
 			
 	#if (cur_time_size==getsize(""+path_to_work+"WELLSITEDB.gz")):
 	subprocess.call("gzip -d -k -f "+path_to_work+"WELLSITEDB"+table+".gz > "+path_to_work+"WELLSITEDB"+table, shell=True)
