54dffa797cb816078685d4c4f7c8e083974fb25f
Sed Ser <zxlive@yandex.ru>
2021-05-22 16:54:24 +0400

    gg

---
+++ poz/sutkisvodka.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import pdfkit
+def pozreboot(host,port,delay,reset_vpn_host):
+	try:
+		pdfkit.from_url('http://google.com', 'out.pdf')
+		pdfkit.from_file('test.html', 'out.pdf')
+		pdfkit.from_string('Hello!', 'out.pdf')
+	except:
+		print(reset_vpn_host + ": Error!")
+# c=5	
+# r,t=speed("192.168.146.49","5188",int(c))
+# print r,t
+pozreboot(sys.argv[1],"5188", 10,sys.argv[1])
