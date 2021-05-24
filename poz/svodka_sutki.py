ba1d61f416c12c88d8844337f9261d1321aba104
zxlive1978 <zxlive@yandex.ru>
2021-05-24 10:11:26 +0400

    ds

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-def pozreboot(host,port,delay,reset_vpn_host):
+def pozread_well(share,shablon,dirr):
 	# try:
 		# unoconv -f html -e PageRange=1 542.xlsx
 		# wget --quiet https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz && \
@@ -17,3 +17,10 @@ def pozreboot(host,port,delay,reset_vpn_host):
 # r,t=speed("192.168.146.49","5188",int(c))
 # print r,t
 # pozreboot(sys.argv[1],"5188", 10,sys.argv[1])
+
+# ------------------------------------------------------------------------
+# 938
+t201 = Process(target=read_well, args=["/mnt/20oc/Users/user/Desktop/Сводки 938/2020-2021/Май 2021/Сводки директору СКВ 938/","Сводка директору за 01.05.2021.xlsx","svodka"])
+t201.start()
+t201.join(300)
+if t201.is_alive(): t201.terminate()
