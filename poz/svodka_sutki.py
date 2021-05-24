e84e71917a08d82f4e663d9eae37896706930a90
zxlive1978 <zxlive@yandex.ru>
2021-05-24 10:41:26 +0400

    ds

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -8,10 +8,10 @@ import shutil
 import subprocess
 
 def read_well(share,shablon,dirr):
-	try:
-		path = sorted(Path('.').glob(share+shablon))
+	# try:
+		path = sorted(Path(share).glob(shablon))
 		print (str(path))
-	except:
+	# except:
 		print ("неудача")
 		# unoconv -f html -e PageRange=1 542.xlsx
 		# wget --quiet https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz && \
