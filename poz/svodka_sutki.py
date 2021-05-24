ed844f9f4b6b2376cb27b2e3daeb2a2f971597c1
zxlive1978 <zxlive@yandex.ru>
2021-05-24 10:37:23 +0400

    ds

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -8,8 +8,8 @@ import subprocess
 
 def pozread_well(share,shablon,dirr):
 	try:
-		a=0
-		#
+		path = sorted(Path('.').glob(share+shablon))
+		print (str(path))
 	except:
 		print ("неудача")
 		# unoconv -f html -e PageRange=1 542.xlsx
@@ -29,7 +29,7 @@ def pozread_well(share,shablon,dirr):
 
 # ------------------------------------------------------------------------
 # 938
-t201 = Process(target=read_well, args=["/mnt/20oc/Users/user/Desktop/Сводки 938/2020-2021/Май 2021/Сводки директору СКВ 938/","Сводка директору за 01.05.2021.xlsx","svodka"])
+t201 = Process(target=read_well, args=["/mnt/20oc/Users/user/Desktop/Сводки 938/2020-2021/Май 2021/Сводки директору СКВ 938/","Сводка директору за *.xlsx","svodka"])
 t201.start()
 t201.join(300)
 if t201.is_alive(): t201.terminate()
