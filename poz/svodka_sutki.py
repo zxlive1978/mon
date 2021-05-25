28de0ad545dc3fe7aec931cfb04cf7b454571a5f
zxlive1978 <zxlive@yandex.ru>
2021-05-25 10:46:23 +0400

    fd

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -20,12 +20,12 @@ def read_well(share,shablon,dirr,skv, lastdir):
 				for name in files:
 					print(share)
 					print(name)
-					if fnmatch.fnmatch(name, shablon):
-						statbuf = os.stat(share+name)
-						if ((statbuf.st_mtime>(time.time()-86400))):
-							names=dirr+'/'+str(datetime.fromtimestamp(statbuf.st_mtime))[:16]+' АГКМ-'+skv+''+'.xlsx'
-							shutil.copy(share+name, names)
-							subprocess.call('unoconv -f html -e PageRange=1 '+names, shell=True)
+					# if fnmatch.fnmatch(name, shablon):
+					# 	statbuf = os.stat(share+name)
+					# 	if ((statbuf.st_mtime>(time.time()-86400))):
+					# 		names=dirr+'/'+str(datetime.fromtimestamp(statbuf.st_mtime))[:16]+' АГКМ-'+skv+''+'.xlsx'
+					# 		shutil.copy(share+name, names)
+					# 		subprocess.call('unoconv -f html -e PageRange=1 '+names, shell=True)
 
 							# subprocess.call('cd "'+dirr+lastdir+'" && ls && mv "' +name+'" "'+name[-15:-5]+skv+'.xlsx"'+' && unoconv -f html -e PageRange=1 "'+name[-15:-5]+skv+'.xlsx"', shell=True)
 
@@ -69,6 +69,12 @@ def read_well(share,shablon,dirr,skv, lastdir):
 
 # 449
 t201 = Process(target=read_well, args=["""/mnt/104oc/СНГС №14/АРХИВЫ СКВАЖИН/Архив скв.№449/Сводки скв.№449/Май 21/""","СКВ №449 *.xlsx","/var/www/html/mon/poz/svodka",'АГКМ-449', '/Май 21/'])
+t201.start()
+t201.join(1000)
+if t201.is_alive(): t201.terminate()
+
+# 449
+t201 = Process(target=read_well, args=["""/mnt/104oc/СНГС №14/АРХИВЫ СКВАЖИН/Архив скв.№449/Сводки скв.№449/""","СКВ №449 *.xlsx","/var/www/html/mon/poz/svodka",'АГКМ-449', '/Май 21/'])
 t201.start()
 t201.join(1000)
 if t201.is_alive(): t201.terminate()
