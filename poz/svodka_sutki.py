ec714740009ce8c937eb97ed86a75df5860bac2c
zxlive1978 <zxlive@yandex.ru>
2021-05-27 09:13:54 +0400

    ds

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -16,7 +16,7 @@ def read_well(share,shablon,dirr,skv, lastdir):
 	# try:
 	# output = subprocess.check_output(['программа', 'аргумент 1', '2'])
 	output = subprocess.check_output("ls -R "+share, stderr=subprocess.STDOUT, shell=True)#.check_output(['ls', "-R", "/mnt/104oc/СНГС №14/АРХИВЫ СКВАЖИН/Архив скв.№449/","/dev/null"])
-	print (output)
+	print (output.split("\n\n"))
 		
 		# subprocess.call('cp -R "'+share+'" "'+dirr+'"', shell=True)
 		# subprocess.call('cp -R "'+share+'" "'+dirr+'"', shell=True)
