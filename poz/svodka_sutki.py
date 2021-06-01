9360b4480f5236f7be890a7c81f7b5a74d784e94
zxlive1978 <zxlive@yandex.ru>
2021-06-01 12:11:56 +0400

    dsa

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -24,7 +24,7 @@ def read_well(share,shablon1,shablon2,dirr,skv):
 				# subprocess.call('cp "'+a+'" "'+dirr+'"', shell=True)
 				statbuf = os.stat(a)
 				if ((statbuf.st_mtime>(time.time()-86400))):
-					names=dirr+'/'+str(datetime.fromtimestamp(statbuf.st_mtime))[:16]+' АГКМ-'+skv+''+'.xlsx'
+					names=dirr+'/'+str(datetime.fromtimestamp(statbuf.st_mtime))[:16]+' '+skv+''+'.xlsx'
 					shutil.copy(a, names)
 					subprocess.call('/usr/bin/unoconv -f html -e PageRange=1 '+'"'+names+'"', shell=True)
 					subprocess.call('/bin/rm '+'"'+names+'"', shell=True)
