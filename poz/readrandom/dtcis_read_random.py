5210dc6976265c46a47d42d73f7aaf13db1d7860
zxlive1978 <zxlive@yandex.ru>
2021-07-21 14:37:07 +0400

    dsa

--- poz/readrandom/dtcis_read_random.py
+++ poz/readrandom/dtcis_read_random.py
@@ -19,18 +19,19 @@ from datetime import datetime
 # from datetime import timezone
 
 def read_well(nametime, table, start, stop, whathdo):
+		nametime="/var/www/html/mon/poz/readrandom/"+nametime
 		# print ('suck')
-		## распаковка архива
-		# zipFile = zipfile.ZipFile(nametime, 'r')
-		# zipFile.extractall()
-		# zipFile.close()
+		# распаковка архива
+		zipFile = zipfile.ZipFile(nametime, 'r')
+		zipFile.extractall()
+		zipFile.close()
 		
-		## удаление dep lst
-		# cur_arch=nametime.replace('.zip','.lst')
-		# os.remove(cur_arch)
-		# cur_arch=nametime.replace('.zip','.dep')
-		# os.remove(cur_arch)
-		## вывод
+		# удаление dep lst
+		cur_arch=nametime.replace('.zip','.lst')
+		os.remove(cur_arch)
+		cur_arch=nametime.replace('.zip','.dep')
+		os.remove(cur_arch)
+		# вывод
 		print (nametime+' '+table+' '+start +' '+ stop+' '+whathdo)
 		# print (nametime, table, start, stop, whathdo)
 
