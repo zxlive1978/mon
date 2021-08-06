f9bd513f877d0fd8565c7efc85fdb6d67d2df2d3
zxlive1978 <zxlive@yandex.ru>
2021-08-06 10:51:48 +0400

    s

--- poz/carot_file_to_db.py
+++ poz/carot_file_to_db.py
@@ -36,8 +36,8 @@ def read_carot(sbor,table):
 		#чтение файл
 		with open(names, "r", encoding = "utf-8" ) as file:
 			for line in file:
-				print( line, end="" )
-						
+				print( line+"/n" )
+
 		# удалить файл
 		# subprocess.call('/bin/rm '+'"'+names+'"', shell=True)
 		
