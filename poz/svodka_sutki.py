775c50d2f93bd90eb6987a83188ea4069be85770
Sedyshev serge <zxlive@yandex.ru>
2021-08-18 13:58:30 +0400

    пп

--- poz/svodka_sutki.py
+++ poz/svodka_sutki.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import fnmatch
 
 def read_well(share,shablon1,shablon2,dirr,skv):
-	dt_now =datetime.utcfromtimestamp(time.time()-5*86400) # date.today()
+	dt_now =datetime.utcfromtimestamp(time.time()-86400) # date.today()
 	#print(datetime.utcfromtimestamp(dt_now))
 	month_now = str(dt_now)[5:7]
 	day_now = str(dt_now)[8:10]
