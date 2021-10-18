265f0017845add97100427d164350620c580b279
Sedyshev Serge <zxlive@yandex.ru>
2021-10-18 13:48:38 +0400

    201 откл чтение глубинки

--- poz/dtcis_read_depth.py
+++ poz/dtcis_read_depth.py
@@ -1172,11 +1172,11 @@ t4450.join(300)
 if t4450.is_alive(): t4450.terminate()
 
 
-#938 (Обработчик)
-t4450 = Process(target=read_well, args=["/mnt/20","s20depth"])
-t4450.start()
-t4450.join(300)
-if t4450.is_alive(): t4450.terminate()
+#201 938 (Обработчик)
+# t4450 = Process(target=read_well, args=["/mnt/20","s20depth"])
+# t4450.start()
+# t4450.join(300)
+# if t4450.is_alive(): t4450.terminate()
 
 #542
 #read_well("/mnt/915","s915")
