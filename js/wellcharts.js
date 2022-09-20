7236dd48408518e58888b151c7a8c139ccf1e9d6
zxlive1978 <zxlive@yandex.ru>
2022-09-20 10:45:48 +0400

    DSAD

--- js/wellcharts.js
+++ js/wellcharts.js
@@ -832,6 +832,60 @@ $(function() {
   chart15.lAxis.setMinMax(-1.1, 400.1); //set y limits
   chart15.draw(); //draw chart  
 }catch(e){}});
+
+
+var chart17;
+$(function() {
+  try{
+  //prepare values
+  data33 = []; 
+  data34 = [];
+  data35 = [];
+  data36 = [];
+  mdata34 = [];
+  mdata35 = [];
+  mldata34 = [];
+  mldata35 = [];
+  for (var t = 0; t < numbs117; t++){
+    data33.push({x: d117[t]["date"]/1, y: d117[t]["value"]});
+	if (t>0){
+		rx1=100*(Number(d117[t]["rx"])-Number(d117[t-1]["rx"]))/(Number(d117[t]["date"]/1) - Number(d117[t-1]["date"]/1));
+		tx1=100*(Number(d117[t]["tx"])-Number(d117[t-1]["tx"]))/(Number(d117[t]["date"]/1) - Number(d117[t-1]["date"]/1));
+	} else {rx1=(100*(d117[t]["rx"]))/1; tx1=(100*(d117[t]["tx"]))/1;}
+	mrx1=(10000*(d117[t]["mtx"]))/100;
+	mtx1=(10000*(d117[t]["mrx"]))/100;
+	mlrx1=((d117[t]["mltx"]))/1;
+	mltx1=((d117[t]["mlrx"]))/1;
+	ml1=(100*(d117[t]["ml"]))/1;
+
+  rx1=rx1/100.0; if (rx1<0.01) {rx1=0;} else {rx1=Math.log(rx1*131.2967)*28.3933;};
+  tx1=tx1/100.0; if (tx1<0.01) {tx1=0;} else {tx1=Math.log(tx1*131.2967)*28.3933;};
+
+	data34.push({x: d117[t]["date"]/1, y: rx1 });
+	data35.push({x: d117[t]["date"]/1, y: tx1 });
+	mdata34.push({x: d117[t]["date"]/1, y: mrx1 });
+	mdata35.push({x: d117[t]["date"]/1, y: mtx1 });
+	mldata34.push({x: d117[t]["date"]/1, y: mlrx1 });
+	mldata35.push({x: d117[t]["date"]/1, y: mltx1 });
+	data36.push({x: d117[t]["date"]/1, y: ml1 });
+	}
+  
+  chart16 = new JSGadget.Chart($("#chart16"), { //create chart
+  trends: [new JSGadget.ATrend({color: "green", width: 1}),new JSGadget.ATrend({color: "Violet", width: 1}),new JSGadget.ATrend({color: "orange", width: 1}),new JSGadget.ATrend({color: "IndianRed", width: 1}),new JSGadget.ATrend({color: "teal", width: 1}),new JSGadget.ATrend({color: "blue", width: 1}),new JSGadget.ATrend({color: "Black", width: 1}),new JSGadget.ATrend({color: "Maroon", width: 1})]
+ });
+ 
+  chart16.trends[0].data = data36;
+  chart16.trends[1].data = data35;
+  chart16.trends[2].data = data34;
+  chart16.trends[3].data = mdata34;
+  chart16.trends[4].data = mdata35;
+  chart16.trends[5].data = data33;
+  chart16.trends[6].data = mldata34;
+  chart16.trends[7].data = mldata35;
+  chart16.bAxis.setMinMax(d117[0]["date"]/1, d117[numbs117-1]["date"]/1); //set x limits
+  chart16.lAxis.setMinMax(-1.1, 400.1); //set y limits
+  chart16.draw(); //draw chart  
+}catch(e){}});
 //function tick() {
 
   //var t2 = new Date(), tz = tz = t2.getTimezoneOffset() * 60;
