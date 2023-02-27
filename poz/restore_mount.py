e4ffd54332e7b4bd29e2c04e453aca8cfb8f477e
zxlive1978 <zxlive@yandex.ru>
2023-02-27 13:59:54 +0400

    438 САРАТОВ

--- poz/restore_mount.py
+++ poz/restore_mount.py
@@ -272,9 +272,10 @@ if t104.is_alive(): t104.terminate()
 
 
 
-#542
+#2103 НЕ МАГА НУРЖАН 542
 #threading.Thread(target=read_well, args=["/mnt/915","s915","//192.168.146.226/d/PetroServices/Database/Online/Store"]).start()
-t631 = Process(target=read_well, args=["/mnt/915","s915","//192.168.146.226/d/PetroServices/Database/Online/Store"])
+# t631 = Process(target=read_well, args=["/mnt/915","s915","//192.168.146.226/d/PetroServices/Database/Online/Store"])
+t631 = Process(target=read_well, args=["/mnt/915","s915","//192.168.146.226/c/MLDIP/Mlsystem2.0/Store"])
 t631.start()
 t631.join(30)
 if t631.is_alive(): t631.terminate()
