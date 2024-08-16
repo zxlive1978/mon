588a65b4edad28bc69f55efe5d7e0caaf1731577
Sedyshev S.M. <zxlive@yandex.ru>
2024-08-16 14:58:47 +0400

    Шлюз Технокома

--- poz/pit_go.py
+++ poz/pit_go.py
@@ -183,7 +183,7 @@ if (r.ret_code==0):
 #average62220 = megamultiping.ping ('62.220.55.149')
 
 #80.247.113.226 Техноком 31.173.139.34
-r = pyping.ping('80.247.113.226')
+r = pyping.ping('31.173.139.34')
 if (r.ret_code==0):
 		average8084=r.avg_rtt
 #average8084 = megamultiping.ping ('80.247.113.226')
