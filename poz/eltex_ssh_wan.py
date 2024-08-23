b59f20bf737c061fe49a72d2b42560084b6247e2
Sedyshev S.M. <zxlive@yandex.ru>
2024-08-23 10:29:55 +0400

    parse data1

--- poz/eltex_ssh_wan.py
+++ poz/eltex_ssh_wan.py
@@ -12,34 +12,41 @@ def ubisignal(host, port, timeout, retry_interval, ap, ubi_ver):
 	retry_interval = float(retry_interval)
 	timeout = int(timeout)
 	timeout_start = time.time()
-	if (ubi_ver=="2"):
-		while time.time() < timeout_start + timeout:
+	while time.time() < timeout_start + timeout:
 			time.sleep(retry_interval)  
 			try:
-				client.connect(host, int(port), username="ubnt",  password="ubnt123", allow_agent=False, look_for_keys=False)
+				client.connect(host, int(port), username="admin",  password="suck", allow_agent=False, look_for_keys=False)
 				remote_conn = client.invoke_shell()
 				output = remote_conn.recv(65535)
-				remote_conn.send("mca-status\n")
+				remote_conn.send("sh ip firewall sessions outside-destination-port 4341\n")
+				time.sleep(retry_interval)
+				remote_conn.send("r")
 				time.sleep(retry_interval)
 				output = remote_conn.recv(65535)
 				data = output.splitlines()
 				remote_conn.send("exit\n")
-				#print data
-                time.sleep(retry_interval)
-		        client.close()
+				data = output.splitlines()
+				time.sleep(retry_interval)
+				client.close()
+				# return data[8:]
+                return data[]
+			except:
+				return -100,-100,-100
+            	#print data
+                
+		        
                 # output = remote_conn.recv(65535)
                 # print data[30].split()[1][:-1]
                 # print data[32].split()[1][:-1]
                 # print data[33].split()[1]
                 #wlanConnections=2 'wlanPollingQuality=90', 'wlanPollingCapacity=80') signal -100..-65
-                if (ap):
-                return data[9][16:], data[65][19:],data[67][20:]
-                else: 
-                return data[25][7:], data[65][19:],data[67][20:]
+                # if (ap):
+                # return data[9][16:], data[65][19:],data[67][20:]
+                # else: 
+                # return data[25][7:], data[65][19:],data[67][20:]
 
-            except:
-		        return -100,-100,-100
+            
+		        
 				
 s=ubisignal(sys.argv[1],"22",10,2, True,"1")
-print s
-
+print ('hello' ,s)
